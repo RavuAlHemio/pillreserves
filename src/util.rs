913578c1020c10cd474568715599bc77070b8b29
Ondřej Hošek <ondra.hosek@gmail.com>
2021-07-29 12:30:54 +0200

    fix parsing decimal numbers, add tests

--- src/util.rs
+++ src/util.rs
@@ -106,17 +106,19 @@ pub(crate) fn parse_decimal(mut text: &str) -> Result<Rational64, ParseDecimalEr
     }
 
     // count dots
-    let dot_count = text.bytes()
-        .filter(|b| *b == b'.')
+    let dot_count = text.chars()
+        .filter(|c| *c == '.')
         .count();
     if dot_count > 1 {
         return Err(ParseDecimalError::TooManyDots(dot_count));
     }
 
     // find position of dot
-    let right_dot_pos = text.find('.')
-        .unwrap_or(text.len());
-    let power_of_ten = text.len() - right_dot_pos;
+    let power_of_ten = if let Some(right_dot_pos) = text.find('.') {
+        text.len() - (right_dot_pos + 1)
+    } else {
+        0
+    };
 
     // remove dot from text
     let text_no_dot = text.replace('.', "");
@@ -136,4 +138,59 @@ pub(crate) fn parse_decimal(mut text: &str) -> Result<Rational64, ParseDecimalEr
     }
 
     Ok(Rational64::new(mantissa, denom))
+}
+
+
+#[cfg(test)]
+mod tests {
+    fn test_parse_decimal(expnum: i64, expden: i64, text: &str) {
+        let rat = super::parse_decimal(text)
+            .unwrap();
+        assert_eq!(expnum, *rat.numer());
+        assert_eq!(expden, *rat.denom());
+    }
+
+    #[test]
+    fn test_parse_no_dot() {
+        test_parse_decimal(0, 1, "0");
+        test_parse_decimal(5, 1, "5");
+        test_parse_decimal(128, 1, "128");
+        test_parse_decimal(-5, 1, "-5");
+        test_parse_decimal(-128, 1, "-128");
+    }
+
+    #[test]
+    fn test_parse_trailing_dot() {
+        test_parse_decimal(0, 1, "0.");
+        test_parse_decimal(5, 1, "5.");
+        test_parse_decimal(128, 1, "128.");
+        test_parse_decimal(-5, 1, "-5.");
+        test_parse_decimal(-128, 1, "-128.");
+    }
+
+    #[test]
+    fn test_parse_leading_dot() {
+        test_parse_decimal(0, 1, ".0");
+        test_parse_decimal(1, 2, ".5");
+        test_parse_decimal(16, 125, ".128");
+        test_parse_decimal(-1, 2, "-.5");
+        test_parse_decimal(-16, 125, "-.128");
+    }
+
+    #[test]
+    fn test_parse_leading_zero_dot() {
+        test_parse_decimal(0, 1, "0.0");
+        test_parse_decimal(1, 2, "0.5");
+        test_parse_decimal(16, 125, "0.128");
+        test_parse_decimal(-1, 2, "-0.5");
+        test_parse_decimal(-16, 125, "-0.128");
+    }
+
+    #[test]
+    fn test_parse_mid_dot() {
+        test_parse_decimal(32, 25, "1.28");
+        test_parse_decimal(64, 5, "12.8");
+        test_parse_decimal(-32, 25, "-1.28");
+        test_parse_decimal(-64, 5, "-12.8");
+    }
 }
