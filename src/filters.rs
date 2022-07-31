d098da4098244e70574904d2bad9db97006d9dea
Ondřej Hošek <ondra.hosek@gmail.com>
2022-08-01 00:54:43 +0200

    only output numerator string if denominator is 1

--- src/filters.rs
+++ src/filters.rs
@@ -7,7 +7,11 @@ pub(crate) fn br<S: ToString>(s: S) -> askama::Result<String> {
 }
 
 pub(crate) fn frac2str(frac: Rational64) -> askama::Result<String> {
-    Ok(format!("{}/{}", frac.numer(), frac.denom()))
+    if *frac.denom() == 1 {
+        Ok(frac.numer().to_string())
+    } else {
+        Ok(format!("{}/{}", frac.numer(), frac.denom()))
+    }
 }
 
 pub(crate) fn frac2float(frac: Rational64) -> askama::Result<f64> {
